716759f4cacb534313d7c345e60fd46170b53ca5
Libertrade01 <midefi@protonmail.com>
2026-07-14 21:30:18 -0300

    Add Libertrade loop favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,7 +36,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" href="/icon-192.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
